6ce3f602f3f65a77fb9c1e7aa815bfb4bb5cec13
Collleen Patin <cpatin@sbcglobal.net>
2018-11-04 18:01:53 -0600

    form submission handling

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
 
   
-  const postButton = document.getElementById('post-btn');
-  postButton.addEventListener('click', store);
+  window.onload = function(){
+  var incidentDataButton = document.getElementById('incidentSubmit');
+ 
+
+  incidentDataButton.addEventListener('click', store);
+
+}
+  //const retieveButton = document.getElementById('retrieve-btn');
+ // retieveButton.addEventListener('click', getIncident);
   
-  const retieveButton = document.getElementById('retrieve-btn');
-  retieveButton.addEventListener('click', getIncident);
-  
-  function store(){
-    const formData = new FormData(document.getElementById('msg-form'));
+  function store(event){
+      event.preventDefault();
+    console.log("here");
+    const formData = new FormData(document.getElementById('incidentData'));
+    
     var jsonObject = {};
     formData.forEach(function(value, key){
       jsonObject[key] = value;
     });
     localStorage.setItem("incident",  JSON.stringify(jsonObject));
      }
-  
+  /*
      function getIncident(){
       console.log("Getting incident");
        var savedIncident = localStorage.getItem("incident");
        console.log(savedIncident);
-     }
+     }*/
   
